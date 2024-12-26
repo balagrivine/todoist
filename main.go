ac8d344f1acda0325dee3531328e9f9af93846f7
balagrivine <balagrivine@gmail.com>
2024-12-26 05:11:52 -0800

    feat: add app description

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import (
+	"github.com/balagrivine/todoist"
+)
+
+func main() {
+}
---
+++ todoist/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2024 grivine
+
+*/
+package main
+
+import "github.com/balagrivine/todoist/cmd"
+
+func main() {
+	cmd.Execute()
+}
